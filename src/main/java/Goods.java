8a8c07bbf28ff8ef4c28d4a07555b4f7f00b85b8
android-zyb <androidzheng@163.com>
2021-10-20 17:37:15 +0800

    提交商品实体类

---
+++ src/main/java/Goods.java
@@ -0,0 +1,12 @@
+public class Goods {
+    public String id;
+    public String goods_name;
+
+    public String getId() {
+        return id;
+    }
+
+    public String getGoods_name() {
+        return goods_name;
+    }
+}
