4f8f4f9750b68c185bf4d7843b2ddd57d6bb73d1
android-zyb <androidzheng@163.com>
2021-10-20 17:31:48 +0800

    提交代码1

--- src/main/java/TestDemo.java
+++ src/main/java/TestDemo.java
@@ -1,6 +1,6 @@
 public class TestDemo {
     public static void main(String[] arg) {
         System.out.println("Jenkins自动化部署学习");
-        System.out.println("Jenkins自动化部署学习1");
+        System.out.println("Jenkins自动化部署学习2");
     }
 }
